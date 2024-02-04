bbb72ed35fe4e8b7338a5ddf8195d53960750009
ArielOhana <140969225+ArielOhana@users.noreply.github.com>
2024-02-04 15:08:30 +0200

    merged

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from './NavBar'
+import { Outlet } from 'react-router-dom'
+
+function Layout() {
+  return (
+    <>
+    <NavBar/>
+    <Outlet/>
+    
+    
+    </>
+    
+  )
+}
+
+export default Layout
